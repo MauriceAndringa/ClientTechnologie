b97d6c1591f109a28b94f05c630f8bd01af14597
Kevin Geubels <kevin.geubels@gmail.com>
2019-11-28 17:15:19 +0100

    Reverse Init

--- USBWebserver v8.6/root/NodePortable/config.bat
+++
@@ -1,2 +0,0 @@
-SET SERVER_PATH="server_root\server.js"
-SET CLIENT_PATH="client_root\index.html"
--- USBWebserver v8.6/root/NodePortable/start_nodejs_server.bat
+++
@@ -1,6 +0,0 @@
-cls
-@echo off
-call config.bat
-start node.exe %SERVER_PATH%
-start "" %CLIENT_PATH%
-start "" %CLIENT_PATH%
