cd1de5bea6514d30c9950597c25b35a62a859e7f
Kevin Geubels <kevin.geubels@gmail.com>
2020-05-21 12:23:50 +0200

    Fixed creating and joining rooms

--- server/server.js
+++ server/server.js
@@ -6,45 +6,105 @@ const socketio = require('socket.io');
 //Defines the path of the client files (including index.html)
 const clientPath = __dirname + '/../client';
 
-var socketList = {};
-
 const app = express();
 app.use(express.static(clientPath));
 
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.sockets.on('connection', function(socket){
-    if (socketList.length >= 2) {
-        console.log('Room is full. Socket ' + socket.id + ' will be kicked');
-        console.log('list length: ' + socketList.length);
-        socket.emit('message', 'Room is full');
-        socket.disconnect();
-    } else {
-        console.log('Socket ' + socket.id + ' connected!');
-        console.log('list length: ' + socketList.length);
-        socketList[socketList.length] = socket.id;
-        //Send message to single client
-        socket.emit('message', 'Successfully connected');
-    }
+const rooms = [];
 
-    //When a user presses the 'send' button
-    socket.on('message', function(inputText){
-        //Emit the message to all the client in the room
-        io.sockets.emit('message', 'User ' + socket.id + ': ' + inputText);
+const findRoom = function(number){
+    let targetRoom = {roomNumber: - 1};
+    for(let i = 0; i < rooms.length; i++){
+        if(rooms[i].roomNumber == number){
+            targetRoom = rooms[i];
+        }
+    }
+    return targetRoom;
+}
+
+
+//User connected to the game
+io.sockets.on('connection', function(socket){
+    console.log("User " + socket.id + " connection!");
+    socket.nickname = "";
+
+    //Creating room
+    socket.on('create room', function(data){
+        console.log("after send: create");
+        let userName = data["username"];
+        let roomNumber = data["room"];
+
+        //Not all fields filled in
+        if(roomNumber == "" || userName == ""){
+            console.log("Please fill in all fields: create");
+            socket.emit('create room', "Please fill in all fields");
+        }
+        //Incorrect room
+        else if(findRoom(roomNumber).roomNumber != -1){
+            console.log("Room already exists!: create");
+            socket.emit('create room', "Room already exists!");
+        }
+        //Set nickname
+        else{
+            socket.nickname = userName;
+            let newRoom = {roomNumber: roomNumber, users: [userName]};
+            rooms.push(newRoom);
+            console.log("Room " + roomNumber + " created!");
+            socket.join(roomNumber);
+            console.log("User " + userName + " connected to room " + roomNumber);
+            socket.emit('start game', true);
+            socket.emit('nickname', userName);
+        }
+
     });
 
-    //Handle the disconnect of an user
-    socket.on('disconnect', function(){
-        console.log('Socket ' + socket.id + ' disconnected...');
-        console.log('list length: ' + socketList.length);
-        //Remove the user from the list of connected users
-        for(var i = socketList.length - 1; i >= 0; i--){
-            if(socketList[i] === socket.id){
-                socketList.splice(i, 1);
-            }
+    //Joining rooms
+    socket.on('join room', function(data){
+        console.log("after send: join");
+        let userName = data["username"];
+        let roomNumber = data["room"];
+
+        //Not all fields filled in
+        if(roomNumber == "" || userName == ""){
+            console.log("Please fill in all fields: join");
+            socket.emit('join room', "Please fill in all fields");
         }
-    })
+        //Incorrect room
+        else if(findRoom(roomNumber).roomNumber == -1){
+            console.log("Room does not exists!: join");
+            socket.emit('join room', "Room does not exists!");
+        }
+        //Room is full
+        else if(findRoom(roomNumber).users.length >= 2){
+            console.log("Room is full")
+            socket.emit('join room', "Room is full");
+        }
+        //Register new user
+        else{
+            //Set nickname
+            socket.nickname = userName;
+            socket.join(roomNumber);
+            findRoom(roomNumber).users.push(userName);
+            console.log("User " + userName + " connected to room " + roomNumber);
+            socket.emit('start game', true);
+            socket.emit('nickname', userName);
+        }
+
+    });
+
+
+
+
+
+    //When a user presses the 'send' button
+    //socket.on('message', function(inputText){
+        //Emit the message to all the client in the room
+        //io.sockets.emit('message', 'User ' + socket.id + ': ' + inputText);
+    //});
+
+
 });
 
 server.on('error', (err) => {
